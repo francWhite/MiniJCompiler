0e1017f437b6981b3666e0ccb38772cbfb5bdc0d
Francisco <francisco.metzger@gmail.com>
2020-12-05 10:41:46 +0100

    builtIn-methods 17.5/40

--- src/main/java/ch/hslu/cobau/minij/code/CodeGenerator.java
+++ src/main/java/ch/hslu/cobau/minij/code/CodeGenerator.java
@@ -1,7 +1,10 @@
 package ch.hslu.cobau.minij.code;
 
 import ch.hslu.cobau.minij.ast.BaseAstVisitor;
+import ch.hslu.cobau.minij.ast.constants.IntegerConstant;
+import ch.hslu.cobau.minij.ast.entity.Procedure;
 import ch.hslu.cobau.minij.ast.entity.Program;
+import ch.hslu.cobau.minij.ast.statement.CallStatement;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,7 +15,7 @@ public class CodeGenerator extends BaseAstVisitor {
     private final Map<String, Integer> localsMap = new HashMap<>();
     private final Stack<String> statementsStack = new Stack<>();
 
-    public String getCode(){
+    public String getCode() {
         return code;
     }
 
@@ -20,11 +23,34 @@ public class CodeGenerator extends BaseAstVisitor {
     public void visit(Program program) {
         program.visitChildren(this);
 
+        String statements = "";
+        while (!statementsStack.isEmpty()) {
+            statements = statementsStack.pop() + statements;
+        }
+
+        code += statements;
+    }
+
+    @Override
+    public void visit(Procedure procedure) {
+        if (procedure.getIdentifier().equals("main")) {
+            BuildMainProcedure(procedure);
+            return;
+        }
+
+        BuildProcedure(procedure);
+    }
+
+
+    private void BuildMainProcedure(Procedure procedure) {
+        procedure.visitChildren(this);
+
         int stackSize = localsMap.size() * 8;
         stackSize += stackSize % 16; // align to 16 bytes
 
         code = "DEFAULT REL\n" +
                 "extern writeInt\n" +
+                "extern writeChar\n" +
                 "extern _exit\n" +
                 "global _start\n" +
                 "section .text\n" +
@@ -34,7 +60,7 @@ public class CodeGenerator extends BaseAstVisitor {
                 "   sub  rsp, " + stackSize + "\n";
 
         String statements = "";
-        while(!statementsStack.isEmpty()) {
+        while (!statementsStack.isEmpty()) {
             statements = statementsStack.pop() + statements;
         }
 
@@ -43,5 +69,48 @@ public class CodeGenerator extends BaseAstVisitor {
                 "   call _exit\n" +
                 "   mov  rsp, rbp\n" +
                 "   pop  rbp\n";
+    }
+
+    private void BuildProcedure(Procedure procedure) {
+        procedure.visitChildren(this);
+
+        //epilog
+        var procedureCode = procedure.getIdentifier() + ":\n" +
+                "   push rbp\n" +
+                "   mov rbp, rsp\n";
+
+        //save parameters
+        //TODO
+
+        //actual procedure code
+        String statements = "";
+        while (!statementsStack.isEmpty()) {
+            statements = statementsStack.pop() + statements;
+        }
+        procedureCode += statements;
+
+        //epilog
+        procedureCode += "   mov rsp, rbp\n" +
+                "   pop rbp\n" +
+                "   ret\n";
+
+        statementsStack.push(procedureCode);
+    }
+
+    @Override
+    public void visit(CallStatement callStatement) {
+        callStatement.visitChildren(this);
+
+        //TODO build all Parameters correctly, currently only 1 param is supported
+        var parameterValue = statementsStack.pop();
+        var procedureCode = "   mov rdi, " + parameterValue + "\n" +
+                "   call " + callStatement.getIdentifier() + "\n";
+
+        statementsStack.push(procedureCode);
+    }
+
+    @Override
+    public void visit(IntegerConstant integerConstant) {
+        statementsStack.push(Long.toString(integerConstant.getValue()));
     }
 }
